89147cc340a58e54d381b3a6846557bd69154af6
Sean Duiser <duzer84@gmail.com>
2022-03-01 09:58:23 -0500

    have a working timer that displays a game over message when it reaches 0

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("JavaScript is working!")
+
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+var secondsLeft = 10;
+
+function setTime() {
+    var timeInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds remaining"
+
+        if(secondsLeft === 0) {
+            clearInterval(timeInterval);
+            sendMessage();
+        }
+    } , 1000);
+}
+
+function sendMessage() {
+    timeEl.textContent = "Time is up! Game Over!";
+}
+
+setTime();
