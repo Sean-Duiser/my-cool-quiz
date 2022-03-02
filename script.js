3c55dc5f7f22eed43913bd4d116e3b8bf2885b72
Sean Duiser <duzer84@gmail.com>
2022-03-01 21:14:40 -0500

    calling it a night. getting more comfortable with navigating the DOM and creating elements

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
 var playTime = document.getElementById("play");
+var endgameEl = document.getElementById("endgame");
 var secondsLeft = 1000;
 
+endgameEl.style.display = 'none';
 var goBtn = document.querySelector('#go');
 
+// questions for my quiz
 var questionOne="What planet do we live on?";
 var questionTwo="How many days are in a Leap Year?";
 var questionThree ="Which animal has stripes on its skin as well as its fur?";
 var questionFour='Which NFL team achieved the "Perfect Season" in 1972?';
 
+// array containing each question for easy access
 var myQuestions = [
     questionOne,
     questionTwo,
@@ -17,6 +21,7 @@ var myQuestions = [
     questionFour
 ]
 
+// answers to each of my questions
 var myAnswers = {
         answersOne: [
             "Earth",
@@ -46,21 +51,20 @@ var myAnswers = {
 
 // this hides the start screen and starts the question
 function displayQuestionOne() {
+    endgameEl.style.display = 'none'
     playTime.style.display = 'none';
     
     // creates a div so that I can append my question to the body
     var questionTime = document.createElement('div');
     questionTime.setAttribute('id', 'divQ1');
-    questionTime.innerHTML = myQuestions.questionOne;
+    questionTime.innerHTML = questionOne;
     document.body.appendChild(questionTime);
-    console.log(questionTime);
     
     // creates another div so I can append my answers to my question
-    var answersOne = document.createElement('div');
-    answersOne.setAttribute('id', 'divA1');
-    answersOne.innerHTML = questionUno.answers;
-    document.body.appendChild(answersOne);
-    console.log(answersOne);
+    var optionsOne = document.createElement('div');
+    optionsOne.setAttribute('id', 'divA1');
+    optionsOne.innerHTML = myAnswers.answersOne;
+    document.body.appendChild(optionsOne);
     
     // this begins the count down
     setTime();
@@ -68,79 +72,79 @@ function displayQuestionOne() {
 };
 
 function displayQuestionTwo() {
+    endgameEl.style.display = 'none'
     document.getElementById('divQ1').style.display = 'none';
     document.getElementById('divA1').style.display = 'none';
     
     // creates a div so that I can append my question to the body
     var questionTime = document.createElement('div');
     questionTime.setAttribute('id', 'divQ2')
-    questionTime.innerHTML = myQuestions.questionTwo;
+    questionTime.innerHTML = questionTwo;
     document.body.appendChild(questionTime);
-    console.log(questionTime);
     
     // creates another div so I can append my answers to my question
-    var answersTwo = document.createElement('div');
-    answersTwo.setAttribute('id', 'divA2');
-    answersTwo.innerHTML = questionDos.answers;
-    document.body.appendChild(answersTwo);
-    console.log(answersTwo);
+    var optionsTwo = document.createElement('div');
+    optionsTwo.setAttribute('id', 'divA2');
+    optionsTwo.innerHTML = myAnswers.answersTwo;
+    document.body.appendChild(optionsTwo);
     
     displayQuestionThree();
 };
 
 function displayQuestionThree() {
+    endgameEl.style.display = 'none'
     document.getElementById('divQ2').style.display = 'none';
     document.getElementById('divA2').style.display = 'none';
     
     // creates a div so that I can append my question to the body
     var questionTime = document.createElement('div');
     questionTime.setAttribute('id', 'divQ3')
-    questionTime.innerHTML = myQuestions.questionThree;
+    questionTime.innerHTML = questionThree;
     document.body.appendChild(questionTime);
-    console.log(questionTime);
     
     // creates another div so I can append my answers to my question
-    var answersThree = document.createElement('div');
-    answersThree.setAttribute('id', 'divA3')
-    var answerThreeKey = ["Yes", "No", "Probz", "No one really knows"];
-    answersThree.innerHTML = questionTres.answers;
-    document.body.appendChild(answersThree);
-    console.log(answersThree);
+    var optionsThree = document.createElement('div');
+    optionsThree.setAttribute('id', 'divA3')
+    optionsThree.innerHTML = myAnswers.answersThree;
+    document.body.appendChild(optionsThree);
     
     displayQuestionFour();
 };
 
 function displayQuestionFour() {
+    endgameEl.style.display = 'none'
     document.getElementById('divQ3').style.display = 'none';
     document.getElementById('divA3').style.display = 'none';
     
     // creates a div so that I can append my question to the body
     var questionTime = document.createElement('div');
     questionTime.setAttribute('id', 'divQ4')
-    questionTime.innerHTML = myQuestions.questionFour;
+    questionTime.innerHTML = questionFour;
     document.body.appendChild(questionTime);
-    console.log(questionTime);
     
     // creates another div so I can append my answers to my question
-    var answersFour = document.createElement('div');
-    answersFour.setAttribute('id', 'divA4');
-    var answerFourKey = ['Always', 'No', 'Wouldn\'t you like to know?', 'What does "cold" mean?'];
-    answersFour.innerHTML = questionQuatro.answers;
-    document.body.appendChild(answersFour);
-    console.log(answersFour);
+    var optionsFour = document.createElement('div');
+    optionsFour.setAttribute('id', 'divA4');
+    optionsFour.innerHTML = myAnswers.answersFour;
+    document.body.appendChild(optionsFour);
     
     endGame()
 };
 
 function endGame() {
+    endgameEl.style.display = 'block'
     document.getElementById('divQ4').style.display = 'none';
     document.getElementById('divA4').style.display = 'none';
     
     var endScreen = document.createElement('div');
     endScreen.setAttribute('id', 'divEnd');
-    var endMessage = "Thank you for taking the quiz. Log your initials below!";
+    var endMessage = "Thank you for taking the quiz. Log your initials above!";
     endScreen.innerHTML = endMessage;
     document.body.appendChild(endScreen);
+    var leaderboard = document.createElement('div');
+    leaderboard.setAttribute('id', 'leaderboard');
+    document.body.appendChild(leaderboard);
+    
 };
 
 // this is the function for the timer to count down
